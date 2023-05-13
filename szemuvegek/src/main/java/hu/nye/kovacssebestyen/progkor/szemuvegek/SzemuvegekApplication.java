65ab7eac17dc91a16c306e98f82e9663dd3a547a
KovacsSebestyen <kovacsseb2002@gmail.com>
2023-05-13 15:59:34 +0200

    Try commit

---
+++ szemuvegek/src/main/java/hu/nye/kovacssebestyen/progkor/szemuvegek/SzemuvegekApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.kovacssebestyen.progkor.szemuvegek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzemuvegekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzemuvegekApplication.class, args);
+	}
+
+}
